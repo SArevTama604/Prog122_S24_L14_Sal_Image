3b804e2f6111cb2a67808fe006e58e751d1953bf
Salvador <sarevalotamayo@student.rtc.edu>
2024-06-14 21:30:06 -0700

    update

---
+++ Art.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_122_S24_L14_Sal
+{
+    public enum STYLE { Cubism, Impressionism, Modernism } 
+
+    public class Art
+    {
+        STYLE _style;
+        string _name;
+
+        public STYLE Style { get => _style; set => _style = value; }
+        public string Name { get => _name; set => _name = value; }
+
+        public Art(string name, STYLE style)
+        {
+            _name = name;
+            _style = style;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} - {_style}";
+        }
+    } // Art
+} // namespace
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace Prog122_S24_L14_Sal_Image
+namespace Prog_122_S24_L14_Sal
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            cmbStyles.ItemsSource = Enum.GetValues(typeof(STYLE));
+
+            Art nighthawks = new Art("Nighthawks", STYLE.Impressionism);
+            runDisplay.Text = nighthawks.ToString();
+        }
+
+        private BitmapImage LoadImage(string filePath)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(filePath);
+            bitmap.EndInit();
+            return bitmap;
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                imgDisplay.Source = LoadImage(selectedFilePath);
+            }
         }
     }
 }
